44d89a5d8228d466ccce862489f3b5b54d0d2f54
zzfima <zzfima@gmail.com>
2022-09-25 07:03:55 +0300

    add impl

---
+++ 40_239_Linked_list/40_239_Linked_list/SingleLinkedList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _40_239_Linked_list
+{
+    public class SingleLinkedList<T>
+    {
+        private Node<T> _head;
+        private Node<T> _tail;
+
+        public SingleLinkedList()
+        {
+            Count = 0;
+            _head = new Node<T>();
+            _tail = _head;
+        }
+
+        public int Count { get; private set; }
+
+        public void AddLast(T i)
+        {
+            var n = new Node<T>();
+            n.Value = i;
+            n.Next = _tail;
+            _tail = n;
+            Count++;
+        }
+    }
+}
--- 40_239_Linked_list/TestProject1/UnitTest1.cs
+++ 40_239_Linked_list/TestProject1/UnitTest1.cs
@@ -7,11 +7,38 @@ namespace TestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void NodeShallReturnValue()
         {
             var n = new Node<int>() { Value = 44 };
 
             n.Value.Should().Be(44);
+        }
+
+
+        [TestMethod]
+        public void LinkedListShallBeEmptyWhenStart()
+        {
+            var s = new SingleLinkedList<int>();
+            s.Count.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void LinkedListShallBe1WhenAdd1()
+        {
+            var s = new SingleLinkedList<int>();
+            s.AddLast(4);
+            s.Count.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void LinkedListShallBe4WhenAdd4()
+        {
+            var s = new SingleLinkedList<int>();
+            s.AddLast(4);
+            s.AddLast(5);
+            s.AddLast(6);
+            s.AddLast(7);
+            s.Count.Should().Be(4);
         }
     }
 }
