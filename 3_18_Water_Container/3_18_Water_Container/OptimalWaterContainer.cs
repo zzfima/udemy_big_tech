60698bbdfba3c43a763f60bb6d4371c4438dec6e
zzfima <zzfima@gmail.com>
2022-09-26 11:03:23 +0300

    slow approach

---
+++ 3_18_Water_Container/3_18_Water_Container/OptimalWaterContainer.cs
@@ -0,0 +1,36 @@
+﻿namespace _3_18_Water_Container
+{
+    public class OptimalWaterContainer
+    {
+        public WaterContainer CalculateAreaSlow(int[] heights)
+        {
+            var maxVolume = new WaterContainer();
+
+            for (int i = 0; i < heights.Length; i++)
+            {
+                for (int j = i + 1; j < heights.Length; j++)
+                {
+                    var area = CalculateAreaOfTrapezoid(heights[i], heights[j], j - i);
+                    if (area > maxVolume.Area)
+                    {
+                        maxVolume.Area = area;
+                        maxVolume.LeftWall = heights[i];
+                        maxVolume.RightWall = heights[j];
+                    }
+
+                }
+            }
+
+            return maxVolume;
+        }
+
+        private int CalculateAreaOfTrapezoid(int a, int b, int h) => (a + b) * h / 2;
+    }
+
+    public class WaterContainer
+    {
+        public int LeftWall { get; set; }
+        public int RightWall { get; set; }
+        public int Area { get; set; }
+    }
+}
---
+++ 3_18_Water_Container/TestProject1/UnitTest1.cs
@@ -0,0 +1,35 @@
+using _3_18_Water_Container;
+using FluentAssertions;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static OptimalWaterContainer _optimalWaterContainer;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            _optimalWaterContainer = new OptimalWaterContainer();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _optimalWaterContainer.CalculateAreaSlow(new[] { 1, 8, 6, 2, 9, 4 })
+                .Should()
+                .BeEquivalentTo(new WaterContainer() { LeftWall = 8, RightWall = 9, Area = 25 });
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            _optimalWaterContainer.CalculateAreaSlow(new[] { 10, 1, 1, 1, 1, 20, 1 })
+                .Should()
+                .BeEquivalentTo(
+                new WaterContainer() { LeftWall = 10, RightWall = 20, Area = 75 });
+
+        }
+    }
+}
