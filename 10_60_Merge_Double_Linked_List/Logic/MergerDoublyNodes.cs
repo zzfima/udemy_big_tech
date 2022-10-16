6a0bb41ffb84419d29fd76f65c4f945380d5e15a
zzfima <zzfima@gmail.com>
2022-10-16 07:58:15 +0300

    start merge brute force: add tail and head

---
+++ 10_60_Merge_Double_Linked_List/Logic/MergerDoublyNodes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Logic
+{
+    public class MergerDoublyNodes<T>
+    {
+        public DoublyLinkedList<T> MergeBruteForce(DoublyNode<T>[] nodes)
+        {
+            DoublyLinkedList<T> doubly = new DoublyLinkedList<T>();
+            DoublyNode<T> head = null;
+            DoublyNode<T> tail = null;
+
+            foreach (var doublyNode in nodes)
+            {
+                if (doublyNode.Previous == null)
+                {
+                    head = doublyNode;
+                }
+
+                if (doublyNode.Next == null)
+                {
+                    tail = doublyNode;
+                }
+            }
+
+            if (head == null)
+            {
+                throw new ArgumentException("no head in list");
+            }
+
+            if (tail == null)
+            {
+                throw new ArgumentException("no tail in list");
+            }
+
+            return doubly;
+        }
+    }
+}
--- 10_60_Merge_Double_Linked_List/TestProject1/UnitTest1.cs
+++ 10_60_Merge_Double_Linked_List/TestProject1/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace TestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodDoublyLinkedList1()
         {
             var doubly = new DoublyLinkedList<int>();
 
@@ -27,7 +27,7 @@ namespace TestProject1
         }
 
         [TestMethod]
-        public void TestMethod2()
+        public void TestMethodDoublyLinkedList2()
         {
             var doubly = new DoublyLinkedList<int>();
 
@@ -44,6 +44,87 @@ namespace TestProject1
             }
 
             cnt.Should().Be(32);
+        }
+
+        [TestMethod]
+        public void ShallFailNoHead()
+        {
+            DoublyNode<int> node1 = new DoublyNode<int>() { Value = 11 };
+            DoublyNode<int> node2 = new DoublyNode<int>() { Value = 12 };
+
+            node1.Next = node2;
+            node1.Previous = node2;
+            node2.Previous = node1;
+            node2.Next = null;
+            
+            var nodes = new DoublyNode<int>[]
+            {
+                node2, node1
+            };
+
+            var merger = new MergerDoublyNodes<int>();
+            Action act = () => merger.MergeBruteForce(nodes);
+            act.Should().Throw<ArgumentException>().WithMessage("no head in list");
+        }
+
+        [TestMethod]
+        public void ShallFailNoTail()
+        {
+            DoublyNode<int> node1 = new DoublyNode<int>() { Value = 11 };
+            DoublyNode<int> node2 = new DoublyNode<int>() { Value = 12 };
+
+            node1.Next = node2;
+            node1.Previous = null;
+            node2.Previous = node1;
+            node2.Next = node1;
+
+            var nodes = new DoublyNode<int>[]
+            {
+                node2, node1
+            };
+
+            var merger = new MergerDoublyNodes<int>();
+            Action act = () => merger.MergeBruteForce(nodes);
+            act.Should().Throw<ArgumentException>().WithMessage("no tail in list");
+        }
+
+
+       // [TestMethod]
+        public void TestMethod5()
+        {
+            DoublyNode<int> node1 = new DoublyNode<int>() { Value = 11 };
+            DoublyNode<int> node2 = new DoublyNode<int>() { Value = 12 };
+            DoublyNode<int> node3 = new DoublyNode<int>() { Value = 13 };
+            DoublyNode<int> node4 = new DoublyNode<int>() { Value = 14 };
+            DoublyNode<int> node5 = new DoublyNode<int>() { Value = 15 };
+
+            node1.Next = node2;
+            node2.Next = node3;
+            node3.Next = node4;
+            node4.Next = node5;
+
+            node5.Previous = node4;
+            node4.Previous = node3;
+            node3.Previous = node2;
+            node2.Previous = node1;
+            node1.Previous = node2;
+
+            var nodes = new DoublyNode<int>[]
+            {
+                node2, node1, node3, node5, node4
+            };
+
+            var merger = new MergerDoublyNodes<int>();
+            var doubly = merger.MergeBruteForce(nodes);
+            var cnt = 11;
+
+            foreach (var i in doubly)
+            {
+                i.Should().Be(cnt);
+                cnt += 1;
+            }
+
+            cnt.Should().Be(16);
         }
     }
 }
